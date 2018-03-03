f537de0f470a987bde33b0df8df6970b8eb51247
Paul Dautry <paul.dautry@gmail.com>
2018-03-03 12:21:10 +0100

    Update vrtual-printer

--- for/virtual-printer/server-files/virtual_printer.py
+++ for/virtual-printer/server-files/virtual_printer.py
@@ -1,1 +1,232 @@
-../src/virtual_printer.py
+#!/usr/bin/env python3
+# -!- encoding:utf8 -!-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#    file: virtual_printer.py
+#    date: 2017-09-26
+#  author: paul.dautry
+# purpose:
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#==============================================================================
+# IMPORTS
+#==============================================================================
+import os
+import io
+from PIL        import Image
+from PIL        import ImageDraw
+from base64     import b64encode
+from datetime   import datetime
+#==============================================================================
+# CLASSES
+#==============================================================================
+##
+## @brief      Exception for signaling invalid ipv4 errors.
+##
+class InvalidIPv4Exception(Exception):
+    pass
+##
+## @brief      Exception for signaling too many bytes errors.
+##
+class TooManyBytesException(Exception):
+    pass
+##
+## @brief      Exception for signaling invalid dpi value errors.
+##
+class InvalidDPIValueException(Exception):
+    pass
+##
+## @brief      Class for machine identification code.
+##
+class MachineIdentificationCode(object):
+    ##
+    ## @brief      Constructs the object.
+    ##
+    ## @param      ip    { parameter_description }
+    ##
+    def __init__(self, ip):
+        super(MachineIdentificationCode, self).__init__()
+        self.w = 64 # matrix width in dots
+        self.h = 8 # matrix heights in dots
+        self.oe = 50 # space between matrices
+        self.od = 20 # space between dots
+        self.mat = [ [ 1 for i in range(0, self.w) ] for i in range(0, self.h) ]
+        self.sn = os.urandom(32)
+        self.__prepare(ip)
+    ##
+    ## @brief      { function_description }
+    ##
+    ## @param      ip    { parameter_description }
+    ##
+    def __convert_ip(self, ip):
+        digits = [ int(e) for e in ip.split('.') ]
+
+        if len(digits) != 4:
+            raise InvalidIPv4Exception
+
+        for e in digits:
+            if e > 0xff or e < 0:
+                raise InvalidIPv4Exception
+
+        return bytes(digits)
+    ##
+    ## @brief      Gets the datetime.
+    ##
+    def __get_datetime(self):
+        t = datetime.now()
+        y1 = t.year // 100
+        y2 = t.year - y1 * 100
+        return bytes([t.day, t.month, y1, y2, t.hour, t.minute, t.second])
+    ##
+    ## @brief      Sets the data.
+    ##
+    ## @param      data  The data
+    ##
+    def __set_data(self, data):
+        if len(data) > self.w:
+            raise TooManyBytesException
+
+        for c in range(0, self.w):
+            if c >= len(data):
+                continue
+
+            o = data[c]
+
+            for r in range(0, self.h):
+                if (o >> r) & 1 == 0:
+                    self.mat[7-r][c] = 0
+    ##
+    ## @brief      { function_description }
+    ##
+    ## @param      ip    { parameter_description }
+    ##
+    def __prepare(self, ip):
+        bip = self.__convert_ip(ip)
+        bdate = self.__get_datetime()
+        data = b'ip:'
+        data += bip
+        data += b'\nat:'
+        data += bdate
+        data += b'\nS/N:'
+        data += self.sn
+        data += b'\n'
+        self.__set_data(data)
+    ##
+    ## @brief      { function_description }
+    ##
+    ## @param      rn    { parameter_description }
+    ## @param      cn    { parameter_description }
+    ##
+    def __points_from_mat(self, rn, cn):
+        points = []
+        for r in range(0, self.h):
+            for c in range(0, self.w):
+                if self.mat[r][c] == 1:
+                    x = c * self.od + self.oe + cn * (self.oe + self.w * self.od)
+                    y = r * self.od + self.oe + rn * (self.oe + self.h * self.od)
+                    points.append((x,y))
+        return points
+    ##
+    ## @brief      { function_description }
+    ##
+    ## @param      img   The image
+    ##
+    def apply_on(self, img):
+        (w, h) = img.size
+        cc = w // (self.w * self.od + self.oe)
+        rc = h // (self.h * self.od + self.oe)
+        d = ImageDraw.Draw(img)
+        for r in range(0, rc):
+            for c in range(0, cc):
+                points = self.__points_from_mat(r,c)
+                d.point(points, fill=(255, 255, 204))
+        return True
+    ##
+    ## @brief      { function_description }
+    ##
+    def b64sn(self):
+        return b64encode(self.sn).decode()
+##
+## @brief      Class for A4 page.
+##
+class A4Page(object):
+    SZ_PER_DPI = {
+        300: (2480, 3508),
+        200: (1654, 2339),
+        100: (827, 1170),
+        72: (596, 842)
+    }
+    WHITE = (255, 255, 255)
+    ##
+    ## @brief      Constructs the object.
+    ##
+    ## @param      margins  The margins
+    ## @param      dpi      The dpi
+    ##
+    def __init__(self, margins, dpi=300):
+        super(A4Page, self).__init__()
+        (self.t, self.l, self.b, self.r) = margins
+        if dpi not in A4Page.SZ_PER_DPI.keys():
+            raise InvalidDPIValueException
+        (self.w, self.h) = A4Page.SZ_PER_DPI[dpi]
+        self.page = Image.new('RGB', A4Page.SZ_PER_DPI[dpi], A4Page.WHITE)
+    ##
+    ## @brief      { function_description }
+    ##
+    ## @param      mic   The mic
+    ##
+    def apply_mic(self, mic):
+        return mic.apply_on(self.page)
+    ##
+    ## @brief      Sets the image.
+    ##
+    ## @param      img   The image
+    ##
+    def set_image(self, img):
+        # resize image if necessary
+        (w, h) = img.size
+        cw = min(self.w / (w + self.l + self.r), 1.0)
+        ch = min(self.h / (h * cw + self.t + self.b), 1.0)
+        nw = int(w * cw * ch)
+        nh = int(h * cw * ch)
+        rimg = img.resize((nw, nh), Image.BICUBIC)
+        # add image to page
+        if len(rimg.split()) > 3:
+            self.page.paste(rimg, (self.l, self.t), mask=rimg.split()[-1])
+        else:
+            self.page.paste(rimg, (self.l, self.t))
+        return True
+    ##
+    ## @brief      { function_description }
+    ##
+    def data(self):
+        data = io.BytesIO()
+        self.page.save(data, format='png')
+        return data
+#==============================================================================
+# FUNCTIONS
+#==============================================================================
+##
+## @brief      { function_description }
+##
+## @param      img_bytes  The image bytes
+## @param      ip         { parameter_description }
+##
+def print_img(img_bytes, ip):
+    page = A4Page((25, 25, 25, 25), 300)
+    mic = MachineIdentificationCode(ip)
+    img = Image.open(io.BytesIO(img_bytes))
+    if not page.set_image(img):
+        return {
+            'status': False,
+            'error': 'invalid image. Try with another (RGB or RGBA) PNG file.'
+        }
+    if not page.apply_mic(mic):
+        return {
+            'status': False,
+            'error': 'failed to apply mic.'
+        }
+    return {
+        'status': True,
+        'data': page.data().getvalue(),
+        'b64sn': mic.b64sn()
+    }
--- for/virtual-printer/server-files/virtual_printer_wrapper.py
+++ for/virtual-printer/server-files/virtual_printer_wrapper.py
@@ -1,1 +1,299 @@
-../src/virtual_printer_wrapper.py
+#!/usr/bin/env python3
+# -!- encoding:utf-8 -!-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#    file: dna_decoder_wrapper.py
+#    date: 2017-08-30
+#  author: paul.dautry
+# purpose:
+#      Wrapper for virtual printer script
+# license:
+#      GPLv3
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# IMPORTS
+#==============================================================================
+import os
+import base64
+import asyncio
+import aioredis
+from ruamel import yaml
+from argparse import ArgumentParser
+from tornado.web import Application, RequestHandler
+from tornado.log import access_log, enable_pretty_logging
+from tornado.options import options, parse_command_line
+from virtual_printer import print_img
+from tornado.platform.asyncio import AsyncIOMainLoop
+#==============================================================================
+# CONFIG
+#==============================================================================
+REDIS_TIMEOUT = 5.0 # seconds
+#==============================================================================
+# CLASSES
+#==============================================================================
+##
+## @brief      Class for main handler.
+##
+class MainHandler(RequestHandler):
+    ##
+    ## @brief      { function_description }
+    ##
+    def get(self):
+        self.write(b"""
+<html>
+    <head>
+        <title>Virtual Printer Service</title>
+    </head>
+    <body>
+        <pre>
++------------------------------------------------------------------------------+
+|                                                                              |
+|                          Virtual Printer Service !                           |
+|                                                                              |
++------------------------------------------------------------------------------+
+|                              ________________                                |
+|                            _/_______________/|                               |
+|                           /___________/___//||   <- yes it's a printer :/    |
+|                          |===        |----| ||                               |
+|                          |           |   o| ||                               |
+|                          |___________|   o| ||                               |
+|                          | ||/.'---.||    | ||                               |
+|                          |-||/_____\||-.  | |'                               |
+|                          |_||=L==H==||_|__|/                                 |
+|                                                                              |
++------------------------------------------------------------------------------+
+|                                                                              |
+|                               INSTRUCTIONS                                   |
+|                                                                              |
++------------------------------------------------------------------------------+
+|                                                                              |
+| HEAD                                                                         |
+|   > gives you the server headers. (useless)                                  |
+|                                                                              |
+|   $> curl -I <url>                                                           |
+|                                                                              |
+| GET                                                                          |
+|   > /whatever/you/want...: gives you this page.                              |
+|                                                                              |
+|   $> curl <url>                                                              |
+|                                                                              |
+| POST                                                                         |
+|   > /print: expects data to be a PNG file (RGB or RGBA).                     |
+|   < you'll get your A4 page printed in the response as raw PNG data.         |
+|                                                                              |
+|   $> curl -X POST --form "f=@<file.png>" <url>                               |
+|                                                                              |
+|   > /serial-number: expects data to be a base64 encoded S/N                  |
+|   < you'll get the flag if you're right                                      |
+|                                                                              |
+|   $> curl -X POST --data "sn=<b64encoded-serial-number>" <url>               |
+|                                                                              |
++------------------------------------------------------------------------------+
+        </pre>
+        <pre style="visibility: hidden;">
+            If y0u w4nn4 l34k, b3 sm4rt & st34lthy.
+        </pre>
+        <pre style="visibility: hidden;">
+            Tested with curl 7.55.1
+        </pre>
+    </body>
+</html>
+""")
+##
+## @brief      Class for print handler.
+##
+class PrintHandler(RequestHandler):
+    ##
+    ## @brief      { function_description }
+    ##
+    def initialize(self, redis_host, redis_port):
+        self.redis_addr = (redis_host, redis_port)
+    ##
+    ## @brief      { function_description }
+    ##
+    def __reject(self, reason):
+        self.set_status(400)    # I'm a teapot
+        self.write({'reason': reason})
+        self.finish()
+    ##
+    ## @brief      { function_description }
+    ##
+    def __accept(self, data):
+        if isinstance(data, str):
+            data = data.encode()
+        self.write(data)
+        self.finish()
+    ##
+    ## @brief      Saves a secret.
+    ##
+    async def __save_secret(self, secret):
+        key = 'vps-' + self.request.remote_ip
+        redis = await aioredis.create_redis(self.redis_addr, timeout=REDIS_TIMEOUT)
+        access_log.info('adding: ({}, {})'.format(key, secret))
+        await redis.set(key, secret, expire=30) # register secret
+        redis.close()
+    ##
+    ## @brief      { function_description }
+    ##
+    async def __print(self):
+        files = self.request.files['f']
+
+        if len(files) != 1:
+            self.__reject("/print expects exactly one image file.")
+            return
+
+        img_data = files[0]['body']
+
+        try:
+            result = print_img(img_data, self.request.remote_ip)
+        except Exception as e:
+            access_log.exception("An exception occured...")
+            self.__reject("[print_img:exception] service failed to process "
+                          "your image. Try with another one.\nIf the problem "
+                          "persists with other images, please contact an "
+                          "admin.")
+            return
+
+        if not result['status']:
+            self.__reject(result['error'])
+            return
+
+        page_data = result['data']
+        secret = result['b64sn']
+
+        try:
+            await self.__save_secret(secret)
+        except Exception as e:
+            access_log.exception("An exception occured...")
+            self.__reject("[__save_secret:exception] service failed to save "
+                          "the secret. Request timed-out.\nPlease contact an "
+                          "admin. Redis server might be down.")
+            return
+
+        self.__accept(page_data)
+    ##
+    ## @brief      { function_description }
+    ##
+    async def post(self):
+        await self.__print()
+##
+## @brief      Class for serial number handler.
+##
+class SerialNumberHandler(RequestHandler):
+    ##
+    ## @brief      { function_description }
+    ##
+    def initialize(self, redis_host, redis_port, flag):
+        self.redis_addr = (redis_host, redis_port)
+        self.flag = flag
+    ##
+    ## @brief      { function_description }
+    ##
+    def __reject(self, reason):
+        self.set_status(400)    # I'm a teapot
+        self.write({'reason': reason})
+        self.finish()
+    ##
+    ## @brief      { function_description }
+    ##
+    def __accept(self, data):
+        if isinstance(data, str):
+            data = data.encode()
+        self.write(data)
+        self.finish()
+    ##
+    ## @brief      Loads a secret.
+    ##
+    async def __load_secret(self):
+        key = 'vps-' + self.request.remote_ip
+        redis = await aioredis.create_redis(self.redis_addr, timeout=REDIS_TIMEOUT)
+        secret = await redis.get(key)
+        redis.close()
+        access_log.info('retrieving: ({}, {})'.format(key, secret))
+        return secret
+    ##
+    ## @brief      { function_description }
+    ##
+    async def __serial_number(self):
+        data = self.request.body
+
+        if data[0:3] == b'sn=':
+            secret = data[3:]
+
+            try:
+                saved_secret = await self.__load_secret()
+            except Exception as e:
+                self.__reject("[__load_secret:exception] service failed to "
+                              "save the secret. Request timed-out.\nPlease "
+                              "contact an admin. Redis server might be down.")
+                return
+
+            if saved_secret is None:
+                self.__reject("Too late, you must submit your serial-number "
+                              "within a timeframe of 25 seconds after your "
+                              "image was printed.")
+                return
+
+            if secret == saved_secret:
+                access_log.info("{} gets a flag!".format(self.request.remote_ip))
+                self.__accept("Good job ! DO NOT LEAK SECRET FILES BY "
+                              "PRINTING THEM ! You can validate with: "
+                              "{}".format(self.flag))
+                return
+
+            self.__reject("Wrong secret... Try again :)")
+            return
+
+        self.__reject("could not find the serial number.\nPlease upload data "
+                      "using the following command: curl -d "
+                      "'sn=<b64encoded_serial_number>' "
+                      "http://...:.../serial-number")
+        return
+    ##
+    ## @brief      { function_description }
+    ##
+    async def post(self):
+        await self.__serial_number()
+#===============================================================================
+# FUNCTIONS
+#===============================================================================
+##
+## @brief      Makes an application.
+##
+def make_app(redis_host, redis_port, flag, debug):
+    return Application([
+        (r"/", MainHandler),
+        (r"/print", PrintHandler, dict(redis_host=redis_host,
+                                       redis_port=redis_port)),
+        (r"/serial-number", SerialNumberHandler, dict(redis_host=redis_host,
+                                                      redis_port=redis_port,
+                                                      flag=flag))
+    ], debug=debug)
+#===============================================================================
+# SCRIPT
+#===============================================================================
+if __name__ == "__main__":
+    p = ArgumentParser(add_help=True, description="Virtual Printer Server.")
+    p.add_argument('-d', '--debug', action='store_true',
+                   help="turns on debug mode.")
+    p.add_argument('-c', '--config', default='.mkctf.yml',
+                   help="mkctf configuration file.")
+    args = p.parse_args()
+
+    options.logging = 'info'
+    parse_command_line(args=[])
+    enable_pretty_logging()
+
+    with open(args.config, 'r') as f:
+        conf = yaml.safe_load(f)
+
+    parameters = conf['parameters']
+
+    AsyncIOMainLoop().install()
+
+    app = make_app(parameters['server']['redis-host'],
+                   parameters['server']['redis-port'],
+                   conf['flag'],
+                   args.debug)
+
+    app.listen(parameters['port'])
+
+    asyncio.get_event_loop().run_forever()
