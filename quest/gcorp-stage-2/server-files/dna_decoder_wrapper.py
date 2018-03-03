69f40e63ab96e9c80685423cde6753d705a86bb6
Paul Dautry <paul.dautry@gmail.com>
2018-03-03 19:26:24 +0100

    Implement build, deploy and update wrapper and exploit.

--- quest/gcorp-stage-2/server-files/dna_decoder_wrapper.py
+++ quest/gcorp-stage-2/server-files/dna_decoder_wrapper.py
@@ -1,1 +1,104 @@
-../src/dna_decoder_wrapper.py
+#!/usr/bin/env python3
+# -!- encoding:utf-8 -!-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#    file: dna_decoder_wrapper.py
+#    date: 2017-08-30
+#  author: paul.dautry
+# purpose:
+#      Stage 2 - G-Corp DNA Decoder
+#         Wrapper for dna_decoder binary
+# license:
+#      GPLv3
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# IMPORTS
+#-------------------------------------------------------------------------------
+import asyncio
+from ruamel import yaml
+from argparse import ArgumentParser
+from subprocess import run
+from subprocess import PIPE
+from tornado.web import Application, RequestHandler
+from tornado.log import access_log, enable_pretty_logging
+from tornado.options import options, parse_command_line
+from tornado.platform.asyncio import AsyncIOMainLoop
+#-------------------------------------------------------------------------------
+# CLASSES
+#-------------------------------------------------------------------------------
+##
+## @brief      Class for main handler.
+##
+class MainHandler(RequestHandler):
+    ##
+    ## @brief      { function_description }
+    ##
+    def get(self):
+        self.write(b"""
+
+                        G-Corp DNA Decoder
+
+    -._    _.--'"`'--._    _.--'"`'--._    _.--'"`'--._    _
+        '-:`.'|`|"':-.  '-:`.'|`|"':-.  '-:`.'|`|"':-.  '.` : '.
+      '.  '.  | |  | |'.  '.  | |  | |'.  '.  | |  | |'.  '.:   '.  '.
+      : '.  '.| |  | |  '.  '.| |  | |  '.  '.| |  | |  '.  '.  : '.  `.
+      '   '.  `.:_ | :_.' '.  `.:_ | :_.' '.  `.:_ | :_.' '.  `.'   `.
+             `-..,..-'       `-..,..-'       `-..,..-'       `         `
+
+
+POST valid DNA data (input limited to 1024 bytes).
+""")
+        self.finish()
+    ##
+    ## @brief      { function_description }
+    ##
+    def post(self):
+        data = self.request.body
+        print(data)
+        # VULN: do not truncate input size
+        try:
+            output = run(['./dna_decoder'], input=data, stdout=PIPE).stdout
+        except Exception as e:
+            access_log.exception("An exception occured...")
+            output = b"Exception raised."
+
+        self.write(output)
+        self.finish()
+#-------------------------------------------------------------------------------
+# FUNCTIONS
+#-------------------------------------------------------------------------------
+##
+## @brief      Makes an application.
+##
+def make_app(debug):
+    return Application([
+        (r"/", MainHandler)
+    ], debug=debug)
+#-------------------------------------------------------------------------------
+# SCRIPT
+#-------------------------------------------------------------------------------
+if __name__ == '__main__':
+    p = ArgumentParser(add_help=True, description="DNA Decoder Server.")
+    p.add_argument('-d', '--debug', action='store_true',
+                   help="turns on debug mode.")
+    p.add_argument('-c', '--config', default='.mkctf.yml',
+                   help="mkctf configuration file.")
+    args = p.parse_args()
+
+    options.logging = 'info'
+    parse_command_line(args=[])
+    enable_pretty_logging()
+
+    with open(args.config, 'r') as f:
+        conf = yaml.safe_load(f)
+
+    parameters = conf['parameters']
+
+    with open('.flag.txt', 'w') as f:
+        f.write(conf['flag'])
+
+    AsyncIOMainLoop().install()
+
+    app = make_app(args.debug)
+
+    app.listen(parameters['port'])
+
+    asyncio.get_event_loop().run_forever()
