de06fd7ada92df43b7f87b9d65dc7e08a3a23fe2
Paul Dautry <paul.dautry@gmail.com>
2018-03-29 18:14:14 +0200

    Integrate and enable

--- prog/notrandom/server-files/server.py
+++ prog/notrandom/server-files/server.py
@@ -1,18 +1,30 @@
-import socket
-from threading import Thread
-from socketserver import ThreadingMixIn
+#!/usr/bin/env python3
+
+import os
 import random
+import socket
 import secrets
 import hashlib
-import os
+from pathlib import Path
+from argparse import ArgumentParser
+from threading import Thread
+from ruamel.yaml import safe_load
+from socketserver import ThreadingMixIn
+
+def get_commit(inst):
+    jackpot = secrets.randbits(10)
+    blind = inst.getrandbits(32)
+    commit = hashlib.md5(str(jackpot+blind).encode()).hexdigest()
+    return [commit,jackpot,blind]
 
 class ClientThread(Thread):
 
-    def __init__(self, ip, port, conn):
+    def __init__(self, ip, port, conn, flag):
         Thread.__init__(self)
         self.ip = ip
         self.port = port
         self.conn = conn
+        self.flag = flag
         self.conn.send("Hello and welcome to \"Will you get your degree\" challenge !  \n".encode())
         self.inst = random.Random()
         self.init_credit = 1000
@@ -34,7 +46,7 @@ class ClientThread(Thread):
                 except ValueError:
                     self.conn.send('Please enter an integer\n'.encode())
                 if(self.init_credit>10000):
-                    self.conn.send((flag+"\n").encode())
+                    self.conn.send((self.flag+"\n").encode())
                     break
                 self.conn.send(("Commitment values : {0:04d} + {1:10d}\n".format(commit_result[1],commit_result[2])).encode())
                 self.conn.send(("You have %d credits remaining\n"%(self.init_credit)).encode())
@@ -43,31 +55,35 @@ class ClientThread(Thread):
                 break
         self.conn.close()
 
-def get_commit(inst):
-    jackpot = secrets.randbits(10)
-    blind = inst.getrandbits(32)
-    commit = hashlib.md5(str(jackpot+blind).encode()).hexdigest()
-    return [commit,jackpot,blind]
+if __name__ == '__main__':
+    p = ArgumentParser()
+    p.add_argument('-c', '--config', type=Path, default=Path('.mkctf.yml'),
+                   help="Configuration file.")
+    args = p.parse_args()
 
+    with open(args.config) as f:
+        conf = safe_load(f)
 
-TCP_IP = '0.0.0.0'
-TCP_PORT = 2004
-flag = open('flag.txt').readline()
+    tcpHost = '0.0.0.0'
+    tcpPort = conf['parameters']['port']
+    flag = conf['flag']
 
-tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcpServer.bind((TCP_IP, TCP_PORT))
-threads = []
+    tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    tcpServer.bind((tcpHost, tcpPort))
+    print("server listening on {}:{}".format(tcpHost, tcpPort))
+    threads = []
 
-try:
-    while True:
-        tcpServer.listen()
-        (conn, (ip, port)) = tcpServer.accept()
-        newthread = ClientThread(ip, port, conn)
-        newthread.start()
-        threads.append(newthread)
+    try:
+        while True:
+            tcpServer.listen()
+            (conn, (ip, port)) = tcpServer.accept()
+            print("new client from {}:{}".format(ip, port))
+            newthread = ClientThread(ip, port, conn, flag)
+            newthread.start()
+            threads.append(newthread)
 
-    for t in threads:
-        t.join()
-except KeyboardInterrupt:
-    os._exit(0)
+        for t in threads:
+            t.join()
+    except KeyboardInterrupt:
+        os._exit(0)
