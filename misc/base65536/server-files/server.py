a0250295386fc40898f6d7adcd4bdefd99a78def
Paul Dautry <paul.dautry@gmail.com>
2018-03-29 18:30:47 +0200

    Integrate and enable

--- misc/base65536/server-files/server.py
+++ misc/base65536/server-files/server.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-from flask import Flask,request
+
 import random
+from flask import Flask,request
+from pathlib import Path
+from argparse import ArgumentParser
+from ruamel.yaml import safe_load
+
 app = Flask(__name__)
+FLAG = None
 
 def is_ascii(s):
     return len(s) == len(s.encode())
@@ -23,18 +29,29 @@ def handle():
         sample = request.form.get('sample')
         if sample and sample!="":
             if is_ascii(sample):
-                flag = open('flag.txt').readline()
                 for _ in range(random.randint(0, 9)):
                     random.shuffle(list_decodable)
                 encrypted_input = encrypt_string(sample,list_decodable)
-                encrypted_flag = encrypt_string(flag,list_decodable)
+                encrypted_flag = encrypt_string(FLAG,list_decodable)
                 return("Your encrypted sample is %s and the flag is %s !\n" % (encrypted_input,encrypted_flag))
             else :
                 return("Only Ascii characters are allowed\n")
         else :
             return ("Param 'sample' is expected\n")
     else:
-        return ("FAIL: Method HTTP not allowed (%s)\n") % (request.method)
+        return ("FAIL: HTTP method not allowed (%s)\n") % (request.method)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    p = ArgumentParser()
+    p.add_argument('-c', '--config', type=Path, default=Path('.mkctf.yml'),
+                   help="Configuration file.")
+    args = p.parse_args()
+
+    with open(args.config) as f:
+        conf = safe_load(f)
+
+    host = '0.0.0.0'
+    port = conf['parameters']['port']
+    FLAG = conf['flag']
+
+    app.run(host=host, port=port)
